ea1a860c15b1e3535dc0a415ddc3df95874a3053
BurAndBY <48630651+BurAndBY@users.noreply.github.com>
2022-02-07 19:35:08 +0300

    First Ungrader release

---
+++ Ungrader.py
@@ -0,0 +1,37 @@
+import os
+import glob
+
+# Sets the main directory
+main_path = input("Enter your scripts path. \n Example: D:/Unity Projects/MyGreatProject/Assets/Scripts/ \n ")
+
+# Gets a list of everything in the main directory including folders
+main_directory = os.listdir(main_path)
+
+# This list will hold all of the folders to search through, including the main folder
+sub_directories = []
+
+# Adds the main folder to to the list of folders
+sub_directories.append(main_path)
+
+# Loops through everthing in the main folder, searching for sub folders
+for item in main_directory:
+    # Creates the full path to each item)
+    item_path = os.path.join(main_path, item)
+
+    # Checks each item to see if it is a directory
+    if os.path.isdir(item_path) == True:
+        # If it is a folder it is added to the list
+        sub_directories.append(item_path)
+
+for directory in sub_directories:
+    for files in glob.glob(os.path.join(directory,"*.cs")): #Only C# files will work
+        f = open( files, 'r' ) #Open files in read-only mode
+        file_contents = f.read() #Read file
+        
+        if "ref " in file_contents: #Contains ref
+            print("Files with ref:", f.name + "; \n") #Type file name
+        if "out var " in file_contents: #Contains out var
+            print(" Files with out var ", f.name + "; \n") #Type file name 
+answer = input("Close app? yes or no") #Ask should we close the app
+if answer in ('yes', 'no'): #Yes is an only option. Accept your faith.
+    exit() #Closing...
